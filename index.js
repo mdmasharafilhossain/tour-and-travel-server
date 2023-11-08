11cda4bd1e1c26155606031879ccfa62f25575e1
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2023-11-08 08:42:41 +0600

    id serching by findone

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 require('dotenv').config()
@@ -42,6 +42,15 @@ app.get('/service', async(req,res)=>{
     const result = await cursor.toArray();
     res.send(result);
 });
+
+
+app.get('/service/:id',async(req,res)=>{
+  const id =req.params.id;
+  const query = {_id : new ObjectId(id)}
+  const result = await serviceCollection.findOne(query);
+  res.send(result);
+  
+})
 
 app.get('/',(req,res)=>{
     res.send('Server Running Successfully');
